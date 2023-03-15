a60f97bfb63c42716966ee0a8934f6301d668125
lam <894363702@qq.com>
2023-03-15 22:07:24 +0800

    完成底部tabbar

---
+++ lib/app/modules/tabs/views/tabs_view.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/tabs_controller.dart';
+
+class TabsView extends GetView<TabsController> {
+  const TabsView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Obx((() => Scaffold(
+          appBar: AppBar(
+            title: const Text('TabsView'),
+            centerTitle: true,
+          ),
+          body: controller.pages[controller.currentIndex()],
+          bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              currentIndex: controller.currentIndex.value,
+              onTap: (value) {
+                controller.setCurrentIndex(value);
+              },
+              selectedItemColor: const Color.fromARGB(255, 255, 145, 0),
+              items: const [
+                BottomNavigationBarItem(icon: Icon(Icons.home), label: "首页"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.category), label: "分类"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.room_service), label: "服务"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.shopping_cart), label: "购物车"),
+                BottomNavigationBarItem(icon: Icon(Icons.people), label: "我的")
+              ]),
+        )));
+  }
+}
